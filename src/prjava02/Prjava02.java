8bdff4cc38d370d1a75c5c59c098adcd37534c9a
almope1516daw2 <almope1516daw2@gmail.com>
2015-11-03 17:16:19 +0100

    .

--- src/prjava02/Prjava02.java
+++ src/prjava02/Prjava02.java
@@ -7,9 +7,7 @@ import java.io.*;
 import java.net.*;
 /**
  *
- * @author ­­> Escriu el teu correu electr
-ò
-nic
+ * @author ­­> Escriu el teu correu electrònic
  */
 public class Prjava02 {
     /**
@@ -17,7 +15,8 @@ public class Prjava02 {
      */
     public static void main(String[] args) {
         System.out.println("versió 0.1 del projecte prjava02"); 
-        try {    
+        try {   
+			 
             InetAddress adreça = InetAddress.getLocalHost();
             String hostname = adreça.getHostName();
             System.out.println("hostname="+hostname);
@@ -28,9 +27,6 @@ public class Prjava02 {
             System.out.println("Creació d'una branca del projecte prjava02");
             System.out.println("Afegint més codi a la branca00 del projecte prjava02");
             System.out.println("Afegnt codi a la branca01 del projecte prjava02");
-
-
-
         }
         catch (IOException e) {
         }  
